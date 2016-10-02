ed3e37ae2f1f33224d3815f7b36830be787f5b76
Omar <theomar3@gmail.com>
2016-10-02 11:05:42 -0500

    bottom section of landing page

--- public/portfolio.js
+++ public/portfolio.js
@@ -1,25 +1,81 @@
-var $ball = $('.ball');
-var $verticalTitle = $('.vertical-titles');
-var $aboutSubHeading = $('.about-sub-heading');
+var $oBall = $('.ball.o');
+var $aboutVerticalTitle = $('.vertical-titles.about');
+var $aboutSubHeading= $('.sub-heading.about');
+
+var $mBall = $('.ball.m');
+var $resumeVerticalTitle = $('.vertical-titles.resume');
+var $resumeSubHeading= $('.sub-heading.resume');
+
+var $aBall = $('.ball.a');
+var $projectsVerticalTitle = $('.vertical-titles.projects');
+var $projectsSubHeading= $('.sub-heading.projects');
+
+var $rBall = $('.ball.r');
+var $socialVerticalTitle = $('.vertical-titles.social');
+var $socialSubHeading= $('.sub-heading.social');
 
 
 
-$ball.mouseover(function() {
-  $ball.toggleClass('animated swing');
+$oBall.mouseover(function() {
+  $oBall.toggleClass('animated swing');
+});
+$oBall.on('click', function() {
+  $oBall.addClass('animated hinge');
+});
+$aboutVerticalTitle.mouseover(function() {
+  $aboutVerticalTitle.toggleClass('animated swing');
+});
+$aboutVerticalTitle.on('click', function() {
+  $oBall.addClass('animated hinge');
+});
+$aboutSubHeading.mouseover(function() {
+  $aboutSubHeading.toggleClass('animated swing');
 });
 
-$ball.on('click', function() {
-  $ball.addClass('animated hinge');
+$mBall.mouseover(function() {
+  $mBall.toggleClass('animated swing');
+});
+$mBall.on('click', function() {
+  $mBall.addClass('animated hinge');
+});
+$resumeVerticalTitle.mouseover(function() {
+  $resumeVerticalTitle.toggleClass('animated swing');
+});
+$resumeVerticalTitle.on('click', function() {
+  $mBall.addClass('animated hinge');
+});
+$resumeSubHeading.mouseover(function() {
+  $resumeSubHeading.toggleClass('animated swing');
 });
 
-$verticalTitle.mouseover(function() {
-  $verticalTitle.toggleClass('animated swing');
+$aBall.mouseover(function() {
+  $aBall.toggleClass('animated swing');
+});
+$aBall.on('click', function() {
+  $aBall.addClass('animated hinge');
+});
+$projectsVerticalTitle.mouseover(function() {
+  $projectsVerticalTitle.toggleClass('animated swing');
+});
+$projectsVerticalTitle.on('click', function() {
+  $aBall.addClass('animated hinge');
+});
+$projectsSubHeading.mouseover(function() {
+  $projectsSubHeading.toggleClass('animated swing');
 });
 
-$verticalTitle.on('click', function() {
-  $ball.addClass('animated hinge');
+$rBall.mouseover(function() {
+  $rBall.toggleClass('animated swing');
 });
-
-$subHeading.mouseover(function() {
-  $subHeading.toggleClass('animated swing');
+$rBall.on('click', function() {
+  $rBall.addClass('animated hinge');
+});
+$socialVerticalTitle.mouseover(function() {
+  $socialVerticalTitle.toggleClass('animated swing');
+});
+$socialVerticalTitle.on('click', function() {
+  $rBall.addClass('animated hinge');
+});
+$socialSubHeading.mouseover(function() {
+  $socialSubHeading.toggleClass('animated swing');
 });

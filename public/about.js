07db5ec1015b600d12811d631ef00f09230380c4
Omar <theomar3@gmail.com>
2016-10-03 19:15:43 -0500

    finished About section

--- public/about.js
+++ public/about.js
@@ -1,1 +1,9 @@
+var $homeText = $('.about-home-link');
+var $homeIcon = $('.home-icon');
 
+$homeText.hover(function() {
+  $homeText.html("<img class='home-icon'src='/images/home-icon.png'/>")
+},
+function() {
+  $homeText.html('No place like it');
+});
